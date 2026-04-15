be932620bb91c1680536fb41895494cd81985e82
Jose <josecondor203@hotmail.com>
2026-04-15 09:29:57 -0500

    V3: implementacion funcional completa

---
+++ Factorial.java
@@ -0,0 +1,8 @@
+// V3: Funcional
+import java.util.stream.LongStream;
+public class Factorial {
+    public static long calcular(int n){
+        return LongStream.rangeClosed(1, n).reduce(1, (a, b) -> a * b);
+    }
+    
+}
